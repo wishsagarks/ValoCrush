7f22ecc0e1056454340e143bb15d68869db008e6
SAGAR SATAPATHY <wishsagarks@gmail.com>
2021-10-31 16:36:35 +0530

    navbar updated

---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function LandingPage() {
+    return (
+        <div>
+            <div className="ui">
+
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
