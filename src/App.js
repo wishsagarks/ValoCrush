2aa6f62d5982ee37f564302b54934ec913882b78
SAGAR SATAPATHY <wishsagarks@gmail.com>
2021-11-18 03:12:39 +0530

    game updated
    css needs to be fixed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import Navbar from './NavBar/NavBar';
 import SideNavPage from './NavBar/SideNavPage';
 import LandingPage from './LandingPage/LandingPage'
-
+import Game from './Game/Game'
 function App() {
   return (
   <>
-    <Navbar/>
+
+  
+    <Navbar/> 
     <SideNavPage/>
+    <Game/>
+  
+   
+   
   </>
 
   
